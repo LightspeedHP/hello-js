17b4b59f2c8eba29f7909bd2b71c21391da1e22f
Michael Akelemor <michaelakelemor@gmail.com>
2026-08-11 22:15:44 +0100

    Added semicolon

--- intro.js
+++ intro.js
@@ -1,3 +1,3 @@
-console.log("My name is Michael Akelemor")
-console.log("I am 22 years old")
-console.log("My favourite programming language is python")
+console.log("My name is Michael Akelemor");
+console.log("I am 22 years old");
+console.log("My favourite programming language is python");
